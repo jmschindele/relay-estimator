4db445d56129bd110ed8a725188b29ee48c96fae
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-07-02 20:56:00 -0400

    database created

--- relay/scripts/seedDB.js
+++ relay/scripts/seedDB.js
@@ -1,84 +1,213 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+console.log(db)
 
-// This file empties the Books collection and inserts the books below
+// This file empties the Projects collection and inserts the projects below
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/reactreadinglist"
+  process.env.MONGODB_URI || "mongodb://localhost/relaydb"
 );
 
 const projectSeed = [
   {
     userName: "Chris1",
-    project: 
-        [
-            {'project1': [
-                {
-                    'task': 'html',
-                    'hours': 4,
-                    'rate': 60
-                },
-                {
-                    'task':'css',
-                    'hours': 12,
-                    'rate': 70
-                },
-                {
-                    'task':'Javascript',
-                    'hours': 20,
-                    'rate': 100
-                },
-                {
-                    'task':'React',
-                    'hours': 10,
-                    'rate': 120
-                },
-                {
-                    'task':'Program Server',
-                    'hours': 20,
-                    'rate': 80
-                }
-            ]},
-            {'project2': [
-                {
-                    'task': 'html',
-                    'hours':10,
-                    'rate': 60
-                },
-                {
-                    'task':'css',
-                    'hours': 20,
-                    'rate': 70
-                },
-                {
-                    'task':'Javascript',
-                    'hours': 30,
-                    'rate': 100
-                },
-                {
-                    'task':'React',
-                    'hours': 40,
-                    'rate': 120
-                },
-                {
-                    'task':'Program Server',
-                    'hours': 50,
-                    'rate': 80
-                }
-            ]},
-        
-        'date': new Date(Date.now())
+    project: [
+      {
+        project1: [
+          {
+            task: "html",
+            hours: 4,
+            rate: 60
+          },
+          {
+            task: "css",
+            hours: 12,
+            rate: 70
+          },
+          {
+            task: "Javascript",
+            hours: 20,
+            rate: 100
+          },
+          {
+            task: "React",
+            hours: 10,
+            rate: 120
+          },
+          {
+            task: "Program Server",
+            hours: 20,
+            rate: 80
+          }
+        ]
+      },
+      {
+        project2: [
+          {
+            task: "html",
+            hours: 10,
+            rate: 60
+          },
+          {
+            task: "css",
+            hours: 20,
+            rate: 70
+          },
+          {
+            task: "Javascript",
+            hours: 30,
+            rate: 100
+          },
+          {
+            task: "React",
+            hours: 40,
+            rate: 120
+          },
+          {
+            task: "Program Server",
+            hours: 50,
+            rate: 80
+          }
+        ],
+        date: new Date(Date.now())
+      }
     ]
-    },
+  },
+  {
+    userName: "Chris2",
+    project: [
+      {
+        project1: [
+          {
+            task: "html",
+            hours: 12,
+            rate: 60
+          },
+          {
+            task: "css",
+            hours: 32,
+            rate: 70
+          },
+          {
+            task: "Javascript",
+            hours: 24,
+            rate: 100
+          },
+          {
+            task: "React",
+            hours: 120,
+            rate: 120
+          },
+          {
+            task: "Program Server",
+            hours: 320,
+            rate: 80
+          }
+        ]
+      },
+      {
+        project2: [
+          {
+            task: "html",
+            hours: 110,
+            rate: 60
+          },
+          {
+            task: "css",
+            hours: 120,
+            rate: 70
+          },
+          {
+            task: "Javascript",
+            hours: 130,
+            rate: 100
+          },
+          {
+            task: "React",
+            hours: 140,
+            rate: 120
+          },
+          {
+            task: "Program Server",
+            hours: 150,
+            rate: 80
+          }
+        ],
+        date: new Date(Date.now())
+      }
+    ]
+  },
+  {
+    userName: "Josh",
+    project: [
+      {
+        project1: [
+          {
+            task: "html",
+            hours: 122,
+            rate: 60
+          },
+          {
+            task: "css",
+            hours: 322,
+            rate: 70
+          },
+          {
+            task: "Javascript",
+            hours: 224,
+            rate: 100
+          },
+          {
+            task: "React",
+            hours: 120,
+            rate: 120
+          },
+          {
+            task: "Program Server",
+            hours: 320,
+            rate: 80
+          }
+        ]
+      },
+      {
+        project2: [
+          {
+            task: "html",
+            hours: 150,
+            rate: 60
+          },
+          {
+            task: "css",
+            hours: 160,
+            rate: 70
+          },
+          {
+            task: "Javascript",
+            hours: 80,
+            rate: 100
+          },
+          {
+            task: "React",
+            hours: 180,
+            rate: 120
+          },
+          {
+            task: "Program Server",
+            hours: 200,
+            rate: 80
+          }
+        ],
+        date: new Date(Date.now())
+      }
+    ]
   }
 ];
 
-db.Book
+db.User
   .remove({})
-  .then(() => db.Book.collection.insertMany(projectSeed))
+  .then(() => db.User.collection.insertMany(projectSeed))
   .then(data => {
-    console.log(data.result.n + " records inserted!");
+    console.log(data.result.n + "records inserted!");
     process.exit(0);
   })
   .catch(err => {
