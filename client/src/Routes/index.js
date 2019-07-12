73249215f9025a608f419d9b2ae4a68dbfd21826
christopher bell <christopherbell@christophers-MacBook-Air.local>
2019-07-11 22:11:21 -0400

    test push

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "../pages/Home/index";
 import NoMatch from "../pages/NoMatch";
-import SignIn from '../components/auth/SignIn';
-import Register from '../components/auth/Register';
+import SignIn from "../components/auth/SignIn";
+import Register from "../components/auth/Register";
 import Chart from "../pages/Graph";
-import Projects from "../pages/Projects"
+import Projects from "../pages/Projects";
 import AppliedRoute from "../components/AppliedRoute";
 
-
-
-export default ({childProps}) =>
-<Switch>
-    <AppliedRoute path='/' exact component={Home} props={childProps}/>
-    <AppliedRoute path='/Home' component={Home}  props={childProps}/>
-    <AppliedRoute path='/signin' component={SignIn} props={childProps}/>
-    <AppliedRoute path='/register' component={Register}  props={childProps}/>
-    <AppliedRoute path='/projects' component={Projects} props={childProps}/>
-    <AppliedRoute path='/estimate' component={Chart}  props={childProps}/>
+export default ({ childProps }) => (
+  <Switch>
+    <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/Home" component={Home} props={childProps} />
+    <AppliedRoute path="/signin" component={SignIn} props={childProps} />
+    <AppliedRoute path="/register" component={Register} props={childProps} />
+    <AppliedRoute path="/projects" component={Projects} props={childProps} />
+    <AppliedRoute path="/estimate" component={Chart} props={childProps} />
     <Route component={NoMatch} />
-</Switch>
+  </Switch>
+);
