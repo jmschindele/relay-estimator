d8f3d26607cb7a77c91f4af92319becc3c68877a
christopher bell <christopherbell@christophers-MacBook-Air.local>
2019-07-18 21:18:42 -0400

    working migrated delete button on project cards

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
+import DeleteBtn from "../DeleteBtn/index";
 
 function ProjectCard(props) {
-
   return (
     <div className="card-p">
       <div className="card-body">
-      <h5 className="card-title">{props.title}</h5>
+        <DeleteBtn onClick={() => props.handleProjectDelete(props._id)} />
+        <h5 className="card-title">{props.title}</h5>
         <hr />
         <p className="card-text">project #1 details</p>
         <hr />
-        <Link to='/tasks'>
+        <Link to="/tasks">
           <button onClick={props.handleProject}>Edit/Add/Remove Tasks</button>
         </Link>
         <hr />
         <Link to="/estimate" className="card-link-p" data={props.data}>
-  
           <button onClick={props.handleProject}>View Graph/Breakdown</button>
         </Link>
       </div>
